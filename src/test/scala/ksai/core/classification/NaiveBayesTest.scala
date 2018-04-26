2c3c68beb1f610a091e29600d4e3d797bbd9350c
nitin.aggarwal <nitin.aggarwal@hpe.com>
2018-04-26 15:56:05 +0530

    Add resource for testing

--- src/test/scala/ksai/core/classification/NaiveBayesTest.scala
+++ src/test/scala/ksai/core/classification/NaiveBayesTest.scala
@@ -1,22 +1,15 @@
 package ksai.core.classification
 
+import ksai.core.classification.NaiveBayesTest.x
 import ksai.data.parser.{ARFF, ARFFParser}
 import ksai.math.Distribution
 import ksai.training.validation.{LOOCV, ValidationImplicits}
 import org.scalatest.{Matchers, WordSpec}
 
 import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
 
 class NaiveBayesTest extends WordSpec with Matchers with ValidationImplicits {
-
-
-
-  val feature: Array[String] = Array(
-    "outstanding", "wonderfully", "wasted", "lame", "awful", "poorly",
-    "ridiculous", "waste", "worst", "bland", "unfunny", "stupid", "dull",
-    "fantastic", "laughable", "mess", "pointless", "terrific", "memorable",
-    "superb", "boring", "badly", "subtle", "terrible", "excellent",
-    "perfectly", "masterpiece", "realistic", "flaws")
 
   "NaiveBayes" should {
 
@@ -47,9 +40,60 @@ class NaiveBayesTest extends WordSpec with Matchers with ValidationImplicits {
     "be able to test of learn method" in {
       pending
       println("batch learn Multinomial")
-
+      val loocv = LOOCV(x.length)
+      var error = 0
+      var total = 0
+      LOOCV.slice(loocv.train, ArrayBuffer)
     }
 
+  }
+
+}
+
+object NaiveBayesTest{
+
+  val feature: Array[String] = Array(
+    "outstanding", "wonderfully", "wasted", "lame", "awful", "poorly",
+    "ridiculous", "waste", "worst", "bland", "unfunny", "stupid", "dull",
+    "fantastic", "laughable", "mess", "pointless", "terrific", "memorable",
+    "superb", "boring", "badly", "subtle", "terrible", "excellent",
+    "perfectly", "masterpiece", "realistic", "flaws")
+
+  val movieX = new Array[Array[Double]](2000)
+  val movieY = new Array[Int](2000)
+
+  val x = new Array[Array[String]](2000)
+
+  val resource = Source.fromFile("src/test/resources/movie.txt").getLines().toArray
+
+  (0 until 2000).foreach { itr =>
+    val value = resource(itr)
+    val words = value.trim.split(" ")
+    println("*****" + words(0))
+    if(words(0).equalsIgnoreCase("pos")){
+      movieY(itr) = 1
+    } else if(words(0).equalsIgnoreCase("neg")) {
+      movieY(itr) = 0
+    } else println("Invalid class label: " + words(itr))
+    x(itr) = words
+  }
+
+  x.indices.foreach{itr =>
+    movieX(itr) = feature(x(itr))
+  }
+
+  //  (0 until 2000).foreach{ iterator =>}
+  val (featureMap, _) = feature.foldLeft((Map.empty[String, Int], 0)){
+    case ((map, k), string) if !map.keySet.contains(string) => (map ++ Map(string -> (k + 1)), k + 1)
+    case (tuple, _) => tuple
+  }
+
+  def feature(x: Array[String]): Array[Double] = {
+    val bag = new Array[Double](featureMap.size)
+    x.foreach{word =>
+      featureMap.get(word).foreach{f=> bag(f) = bag(f) + 1}
+    }
+    bag
   }
 
 }
