b65328cb0e605fbb4f3c976ddbd29cb1366d3bb2
Jain, Akshansh <akshansh.jain@knoldus.in>
2018-08-13 09:16:30 +0530

    removed private modifier from labelMap in Attributes.scala

--- src/main/scala/ksai/data/parser/Attributes.scala
+++ src/main/scala/ksai/data/parser/Attributes.scala
@@ -22,7 +22,7 @@ case class ARFF[A](
   }
 
   def getNumericsForRegressions = {
-    target.map{
+    target.map {
       case t: String => t.toDouble
       case _ => throw new IllegalArgumentException("IT cannot be anything apart from string")
     }
@@ -35,7 +35,7 @@ case class Delimited[A](
                          data: List[Array[Double]] = Nil,
                          target: List[A] = Nil) {
 
-  /*private */def labelMap: Map[A, Int] = {
+  def labelMap: Map[A, Int] = {
     labels.zipWithIndex.map { case (l, index) => l -> index }.toMap
   }
 
